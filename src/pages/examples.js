bc0e47cc692e9696ccd4259e4d660eacc1d23986
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-01-11 10:34:13 +0400

    page query

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Header from '../examples/Header';
 import Layout from '../components/Layout';
-const examples = () => {
+import { graphql } from 'gatsby';
+const examples = ({ data }) => {
+	const { site: { info: { author } } } = data;
 	return (
 		<div>
 			<Layout>
 				<h1>hello from examples page</h1>
 				<Header />
 			</Layout>
+			<h5>{author}</h5>
 		</div>
 	);
 };
-
+export const data = graphql`
+	query MyQuery {
+		site {
+			info: siteMetadata {
+				author
+				data
+				description
+				person {
+					age
+					name
+				}
+				title
+			}
+		}
+	}
+`;
 export default examples;
